d2053f0f91afb204dc9e3d97a0beffcb37874011
Shanza-Rafique <shanzarafique645@gmail.com>
2025-12-11 09:43:07 +0500

    single_thrice.dart

--- single_thrice.dart
+++ single_thrice.dart
@@ -1,3 +1,16 @@
+/*Design the logic of a single-player Dice Game in Flutter (using Dart) as described below. You
+need to design a screen with a button and a Text widget. When the button is clicked, a player’s
+turn generates a random number from 1 to 6 [hint: int number = Random().nextInt(6) + 1;].
+There is a total of 5 turns. The player’s winning combinations is a list of 3 numbers: [2, 4, 6]. We
+need to log each turn of the player into a list containing his past dice numbers. If the person
+gets a 6 in three consecutive turns, he is declared winner and the game ends. If the person’s at
+least 3 dice numbers in the past dice numbers list match with his winning combinations, he
+wins and the game ends. If none of the above two happens till 5 rounds limit, then the person is
+considered winner if the total sum of his dice numbers is greater than 15. If none of the winning
+condition occurs, the person loses. Write backend logic of onPressed function calling a function
+with prototype: void makeTurn(). Also write other logic/functions of StatefulWidget. Print
+messages in console like “Player 1 wins” or “Player 1 loses” using print function. */
+
 import 'dart:math'; 
 import 'package:flutter/material.dart'; 
 void main() { 
@@ -77,3 +90,4 @@ class _DiceGameState extends State<DiceGame> {
     ); 
   } 
 } 
+
